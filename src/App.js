eb18619a336640d419c7de3b0d988eef40f00d37
Zeshan Butt <Zeshan.butt3331@gmail.com>
2022-12-28 21:13:47 +0500

    Added onChange and OnSubmit functions

--- src/App.js
+++ src/App.js
@@ -20,18 +20,20 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/profile" element={<PrivateRoute />}>
-          <Route path="/profile" element={<Profile />} />
+            <Route path="/profile" element={<Profile />} />
           </Route>
           <Route path="/sign-in" element={<SignIn />} />
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/Offers" element={<Offers />} />
-          <Route path="/create-listing" element={<CreateListing />} />
+          <Route path='/create-listing' element={<PrivateRoute />}>
+            <Route path="/create-listing" element={<CreateListing />} />
+          </Route>
         </Routes>
       </Router>
       <ToastContainer
         position="bottom-center"
-        autoClose={5000}
+        autoClose={3000}
         hideProgressBar={false}
         newestOnTop={false}
         closeOnClick
