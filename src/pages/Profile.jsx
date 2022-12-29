4af8b6fd3d691c6c06e6436d5f1c5b30b1509592
Zeshan Butt <Zeshan.butt3331@gmail.com>
2022-12-29 19:55:56 +0500

    Fetched data from db and displayed in profile sec

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,15 +1,18 @@
 import { getAuth, updateProfile } from "firebase/auth";
-import { doc, updateDoc } from "firebase/firestore";
+import { collection, doc, getDocs, orderBy, query, updateDoc, where } from "firebase/firestore";
 import { db } from '../firebase';
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
 import { toast } from "react-toastify";
 import { FcHome } from "react-icons/fc"
 import { Link } from "react-router-dom";
+import ListingItem from "../components/ListingItem";
 export default function Profile() {
 	const auth = getAuth()
 	const navigate = useNavigate()
 	const [changeDetail, setChangeDetail] = useState(false)
+	const [listings, setListings] = useState(null)
+	const [loading, setLoading] = useState(true)
 	const [formData, setFormData] = useState({
 		name: auth.currentUser.displayName,
 		email: auth.currentUser.email,
@@ -39,14 +42,34 @@ export default function Profile() {
 					name,
 				})
 			}
-
 			toast.success("Profile updated sucessfully!")
-
 		} catch (error) {
 			toast.error("Could not update your profile!")
 		}
 	}
 
+	useEffect(() => {
+		async function fetchUserListings() {
+		  const listingRef = collection(db, "listings");
+		  const q = query(
+			listingRef,
+			where("userRef", "==", auth.currentUser.uid),
+			orderBy("timestamp", "desc")
+		  );
+		  const querySnap = await getDocs(q);
+		  let listings = [];
+		  querySnap.forEach((doc) => {
+			return listings.push({
+			  id: doc.id,
+			  data: doc.data(),
+			});
+		  });
+		  setListings(listings);
+		  setLoading(false);
+		  console.log(listings);
+		}
+		fetchUserListings();
+	  }, [auth.currentUser.uid]);
 	return (
 		<>
 			<section className='max-w-6xl flex justify-center items-center mx-auto'>
@@ -82,8 +105,24 @@ export default function Profile() {
 							Sell or rent your home
 						</Link>
 					</button>
-				</div> 
+				</div>
 			</section>
+			<div className="max-w-6xl px-3 mt-6 mx-auto">
+				{!loading && listings.length > 0 && (
+					<>
+						<h2 className="text-2xl text-center font-semibold mb-6">My Listings</h2>
+						<ul className="sm:grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5">
+							{listings.map((listing) => (
+								<ListingItem
+								key={listing.id}
+								id={listing.id}
+								listing={listing.data}
+								/>
+							))}
+						</ul>
+					</>
+				)}
+			</div>
 		</>
 	)
 }
