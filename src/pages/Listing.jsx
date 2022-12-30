cd940834160308fed848b27dd1680666c9302162
Zeshan Butt <Zeshan.butt3331@gmail.com>
2022-12-30 23:03:13 +0500

    Added Share copy functionality

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -3,51 +3,66 @@ import { useEffect, useState } from "react"
 import { useParams } from "react-router";
 import { db } from "../firebase";
 import Spinner from "../components/Spinner"
-import {Swiper, SwiperSlide} from "swiper/react";
-import  SwipeCore, {EffectFade, Autoplay, Navigation, Pagination } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import SwipeCore, { EffectFade, Autoplay, Navigation, Pagination } from "swiper";
 import "swiper/css/bundle"
+import{FaShare} from "react-icons/fa"
 
 export default function Listing() {
     const params = useParams()
     const [listing, setListing] = useState(null)
-    const [loading, setLoading] = useState( true)
+    const [loading, setLoading] = useState(true)
+    const [shareListCopied, setShareListCopied] = useState(false)
     SwipeCore.use([Autoplay, Navigation, Pagination])
-    useEffect(()=>{
-        async function fetchListing(){
+    useEffect(() => {
+        async function fetchListing() {
             const docRef = doc(db, "listings", params.listingId)
             const docSnap = await getDoc(docRef)
-            if(docSnap.exists()){
+            if (docSnap.exists()) {
                 setListing(docSnap.data());
-                setLoading(false);                
-            }             
+                setLoading(false);
+            }
         }
         fetchListing();
-    },[params.listingId, loading])
-    if(loading){
-        return <Spinner/>
+    }, [params.listingId, loading])
+    if (loading) {
+        return <Spinner />
     }
-  return (
-    <main>
-        <Swiper
-        slidesPerView={1}
-        navigation
-        pagination={{ type: "progressbar" }}
-        effect="fade"
-        modules={[EffectFade]}
-        autoplay={{ delay: 3000 }}
-      >
-        {listing.imgUrls.map((url, index) => (
-          <SwiperSlide key={index}>
-            <div
-              className="relative w-full overflow-hidden h-[300px]"
-              style={{
-                background: `url(${listing.imgUrls[index]}) center no-repeat`,
-                backgroundSize: "cover",
-              }}
-            ></div>
-          </SwiperSlide>
-        ))}
-      </Swiper>
-    </main>
-  )
+    return (
+        <main>
+            <Swiper
+                slidesPerView={1}
+                navigation
+                pagination={{ type: "progressbar" }}
+                effect="fade"
+                modules={[EffectFade]}
+                autoplay={{ delay: 3000 }}
+            >
+                {listing.imgUrls.map((url, index) => (
+                    <SwiperSlide key={index}>
+                        <div
+                            className="relative w-full overflow-hidden h-[300px]"
+                            style={{
+                                background: `url(${listing.imgUrls[index]}) center no-repeat`,
+                                backgroundSize: "cover",
+                            }}
+                        ></div>
+                    </SwiperSlide>
+                ))}
+            </Swiper>
+            <div 
+            onClick={()=>{
+                navigator.clipboard.writeText(window.location.href)
+                setShareListCopied(true)
+                setTimeout(()=>{
+                    setShareListCopied(false)
+                },2000)
+            }}
+            className="fixed top-[13%] right-[3%] z-10 bg-white cursor-pointer border-2 border-gray-400 rounded-full w-12 h-12 flex justify-center items-center">
+                <FaShare className="text-lg text-slate-500 " />
+            </div>
+
+            {shareListCopied && <p className="fixed top-[23%] right-[5%] font-semibold border-2 border-gray-400 rounded-md bg-white z-10 p-2">Linked Copied</p>}
+        </main>
+    )
 }
