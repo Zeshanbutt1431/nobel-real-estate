3c992fb3146e79602e1bcc02fce1df1198826e98
Zeshan Butt <Zeshan.butt3331@gmail.com>
2023-01-06 20:35:15 +0500

    Fetched listings for offers page & added Load btn

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ import PrivateRoute from './components/PrivateRoute';
 import CreateListing from './pages/CreateListing';
 import Editlisting from './pages/Editlisting';
 import Listing from './pages/Listing';
+import Category from './pages/Category';
 
 function App() {
   return (
@@ -28,6 +29,7 @@ function App() {
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/forgot-password" element={<ForgotPassword />} />
           <Route path="/offers" element={<Offers />} />
+          <Route path="/category/:categoryName" element={<Category/>} />
           <Route path="/category/:categoryName/:listingId" element={<Listing />} />
           <Route path='/create-listing' element={<PrivateRoute />}>
             <Route path="/create-listing" element={<CreateListing />} />
